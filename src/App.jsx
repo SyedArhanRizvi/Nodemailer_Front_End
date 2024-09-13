c1b0a1489a3c4a0dd6d1370bbc0e0fbeafa3c8eb
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2024-09-13 23:16:10 +0530

    Adding Whole code

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+import axios from "axios"
+import toast from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
+function App() {
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [msg, setMsg] = useState('');
+  const emailHandler = async (e) =>{
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/emailSend", {name, mail, phone, msg});
+      console.log(res)
+      if(res.data.success == true) {
+        toast.success("Email has been send successfully");
+      }
+      
+    } catch (error) {
+      console.log("We cant fetch your api due to this error ", error);
+      
+    }
+  }
+  return (
+    <>   
+     <section className='body'>
+    <div className="title"><h1>Fill the all form fields..</h1></div>
+    <div className="main">
+      <form onSubmit={emailHandler}>
+      <input type="text" placeholder='Enter Your Name here...' name='name' onChange={(e) => setName(e.target.value)} />
+      <input type="text" placeholder='Enter Your mail here...'name='mail' onChange={(e) => setMail(e.target.value)}/>
+      <input type="text" placeholder='Enter Your Phone here...'name='phone' onChange={(e) => setPhone(e.target.value)}/>
+      <input type="text" placeholder='Enter Your message here...'name='msg' onChange={(e) => setMsg(e.target.value)}/>
+      <input type="submit" value={"Submit"} className='btn'/>
+      </form>
+
+    </div>
+
+    </section>
+    <Toaster/>
+    </>
+
+  )
+}
+
+export default App
